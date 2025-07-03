88f519a883e285f548672f61ce19389411128913
Rickersson <henrique.senacnig@gmail.com>
2025-07-02 23:10:12 -0300

    Corrige importação do express

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
 export async function createNestServer(module: any) {
-  const expressApp = express();
-  const app = await NestFactory.create(module, new ExpressAdapter(expressApp));
-  app.enableCors(); 
+  const server = express();
+  const app = await NestFactory.create(module, new ExpressAdapter(server));
+  app.enableCors();
   await app.init();
-  return expressApp;
+  return server;
 }
