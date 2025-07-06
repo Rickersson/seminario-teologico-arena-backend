d7f25a23e7a8d412e4ebf93b8c72145cc0f52f01
Rickersson <henrique.senacnig@gmail.com>
2025-07-06 02:45:58 -0300

    tentativa de correção de enablecors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import express from 'express';
 export async function createNestServer(module: any) {
   const server = express();
   const app = await NestFactory.create(module, new ExpressAdapter(server));
-  app.enableCors();
+  app.enableCors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+});
   await app.init();
   return server;
 }
